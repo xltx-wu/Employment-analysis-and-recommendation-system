8ddbfaa3c45716752b88044d992e5d7503e727b4
是小亮同学 <2463397077@qq.com>
2022-01-01 15:59:41 +0800

    二次测试

---
+++ main.c
@@ -0,0 +1,14 @@
+/*************************************************
+ * Descripttion :
+ * version      : 1.0
+ * Author       : 伍永亮
+ * Date         : 2022-01-01 15:58:47
+ * LastEditors  : 伍永亮
+ * LastEditTime : 2022-01-01 15:58:49
+ **************************************************/
+//测试
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
