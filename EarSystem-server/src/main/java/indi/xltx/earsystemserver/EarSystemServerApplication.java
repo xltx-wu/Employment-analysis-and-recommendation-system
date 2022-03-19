a7effadaabbc5aca0ea0abe2ebd2bf24c7816965
是小亮同学 <2463397077@qq.com>
2022-03-19 21:39:04 +0800

    测试.ignore文件

---
+++ EarSystem-server/src/main/java/indi/xltx/earsystemserver/EarSystemServerApplication.java
@@ -0,0 +1,13 @@
+package indi.xltx.earsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarSystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EarSystemServerApplication.class, args);
+    }
+
+}
---
+++ EarSystem-server/src/test/java/indi/xltx/earsystemserver/EarSystemServerApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.xltx.earsystemserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarSystemServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
